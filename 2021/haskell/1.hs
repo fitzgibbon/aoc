60ae32630562e5fda6e3f12c7fba24e9b168810b
Niall FitzGibbon <fitzgibbon.niall@gmail.com>
2021-12-01 17:15:39 +0000

    1a

--- 2021/haskell/1.hs
+++ 2021/haskell/1.hs
@@ -1,7 +1,15 @@
 import System.IO
 
-readInput:: IO [Char]
+readInput :: IO [Char]
 readInput = readFile "1.input"
 
+getDepths :: [Char] -> [Int]
+getDepths input = read <$> (lines input)
+
+countIncrements :: [Int] -> Int
+countIncrements (x:xs) = fst $ foldr (\y (c, prev) -> (if y <= prev then c + 1 else c, y)) (0, x) xs
+
 main :: IO ()
-main = readInput >>= print
+main = do
+  input <- readInput
+  print $ (countIncrements . getDepths) input
